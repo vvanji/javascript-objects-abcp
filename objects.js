3b98703fd6f4a096ea4412575c301cc7ef857589
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-13 15:55:21 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  artistNames: 'someone name artist',
+  songTitle: 'some song title',
+
+};
+
+function updatePlaylist(playlist, artistNames,  songTitle){
+  Object.assign({}, {playlist: 'someplaylist'})
+}
+
