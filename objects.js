06c52e1006b8763c7999bd9bb1377a19f51c6275
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-13 16:09:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,14 @@
 function updatePlaylist(playlist, artist, song) {
 
   return Object.assign({}, playlist, { [artist]: song })
-  //return Object.assign(obj, { [key]: value })
 }
 
 const playlist = { artist: 'Kesha', song: 'Praying'};
 
 console.log(updatePlaylist(playlist, 'Lady Gaga', 'Million Reasons'));
-// returns `{ eggs: 3, chocolate: '1 cup' }`
 
-playlist // { eggs: 3 }
+
+playlist 
 
 console.log(playlist);
-//console.log(updateObjectWithKeyAndValue);
+
